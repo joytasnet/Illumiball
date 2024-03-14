b43e86e5846517c4d12619a608f7428cacb5ae9f
mjpurin <mjpurin@gmail.com>
2024-03-14 14:20:39 +0900

    p144

--- Assets/Hole.cs
+++ Assets/Hole.cs
@@ -6,19 +6,31 @@ using UnityEngine;
 public class Hole : MonoBehaviour
 {
     public string targetTag;
+    bool isHolding;
 
     void OnTriggerStay(Collider other) {
         Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
-
         Vector3 direction = other.gameObject.transform.position - transform.position;
         direction.Normalize();
-        
         if(other.CompareTag(targetTag)){
             r.velocity *= 0.9f;
             r.AddForce(direction * -20f,ForceMode.Acceleration);
         }else{
             r.AddForce(direction * 80f,ForceMode.Acceleration);
+        }
+    }
 
+    public bool IsHolding(){
+        return isHolding;
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag(targetTag)){
+            isHolding=true;
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if(other.CompareTag(targetTag)){
+            isHolding=false;
         }
     }
 }
